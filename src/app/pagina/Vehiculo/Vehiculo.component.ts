4bae832e6904f627e4482193b9770a54a6fa28ce
OdalisMG <Godalis64gmail.com>
2023-03-09 20:23:44 -0500

    9/3/2023

--- src/app/pagina/Vehiculo/Vehiculo.component.ts
+++ src/app/pagina/Vehiculo/Vehiculo.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { VehiculoService } from 'src/app/servicios/Vehiculo.service';
 @Component({
-  selector: 'app-Vehiculo',
-  templateUrl: './Vehiculo.component.html',
-  styleUrls: ['./Vehiculo.component.css']
+  selector: 'app-vehiculos',
+  templateUrl: './vehiculos.component.html',
+  styleUrls: ['./vehiculos.component.css']
 })
-export class VehiculoComponent implements OnInit {
-
-  constructor() { }
+export class VehiculosComponent implements OnInit {
+  constructor(private vehiculoService:VehiculoService) { }
+  filtrarPor: String = "";
+  listaVehiculo:any[]=[];
+  mostrarImagen:boolean=false;
 
   ngOnInit() {
-  }
-
+    this.listaVehiculo= this.vehiculoService.getVehiculos();
+  }
 }
