88eeb9c8007723fe4702b8acfe702b3e2b248f9b
OdalisMG <Godalis64gmail.com>
2023-03-23 19:48:02 -0500

    22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VehiculosComponent } from './pagina/Vehiculo/Vehiculo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-   
+    VehiculosComponent
   ],
   imports: [
     BrowserModule,
