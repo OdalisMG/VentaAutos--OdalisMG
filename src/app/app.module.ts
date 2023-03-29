5e0ca18a46802d462aff4f43bde0da062cf1b39c
OdalisMG <Godalis64gmail.com>
2023-03-29 15:11:06 -0500

    29/3/2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { VehiculoComponent } from './pagina/Vehiculo/Vehiculo.component';
+import { VehiculosComponent } from './paginas/Vehiculos/Vehiculos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VehiculoComponent
+    VehiculosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/paginas/Vehiculos/Vehiculos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Vehiculos',
+  templateUrl: './Vehiculos.component.html',
+  styleUrls: ['./Vehiculos.component.css']
+})
+export class VehiculosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
